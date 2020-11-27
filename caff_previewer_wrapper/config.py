95036ba44dc364be18d5a8f43bda8e59fdd17d55
marcsello <punkosdmarcell@rocketmail.com>
2020-11-27 19:03:11 +0100

    Fixed configurations

--- caff_previewer_wrapper/config.py
+++ caff_previewer_wrapper/config.py
@@ -4,8 +4,8 @@ import os
 class Config:
     CAFF_PREVIEWER_BINARY = os.environ.get('CAFF_PREVIEWER_BINARY', '/usr/local/bin/caff_previewer')
     IMAGEMAGICK_CONVERT_BINARY = os.environ.get('IMAGEMAGICK_CONVERT_BINARY', '/usr/bin/convert')
-    CONVERSION_TIMEOUT = int(os.environ.get('IMAGEMAGICK_CONVERT_BINARY', 30))
-    RECIEVE_CHUNKSIZE = int(os.environ.get('RECIEVE_CHUNKSIZE', 2048))
+    CONVERSION_TIMEOUT = int(os.environ.get('CONVERSION_TIMEOUT', 30))
+    RECIEVE_CHUNKSIZE = int(os.environ.get('RECIEVE_CHUNKSIZE', 4096))
     MAX_RECIEVE_SIZE = int(os.environ.get('MAX_RECIEVE_SIZE', 536870912))  # 512 MB
     SENTRY_DSN = os.environ.get('SENTRY_DSN')
     RELEASE_ID = os.environ.get('RELEASE_ID', 'test')
